71dcadc5eeba148c0eb93b8ebb29027ed45f4568
alexander <alexandernyaga1999@gmail.com>
2023-11-10 11:35:13 +0300

    Handle submit

--- src/AddBlog.js
+++ src/AddBlog.js
@@ -3,7 +3,20 @@ import React, { useState } from 'react';
 const AddBlog = ({ onAdd }) => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    
+        // Validate form inputs
+        if (!title || !content) {
+          alert('Please fill in all fields.');
+          return;
+        }
 }
+
+}
+
+
 
 
 
