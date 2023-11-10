b91ba93728f9c5033ff877de4831bd53bdc29c6b
alexander <alexandernyaga1999@gmail.com>
2023-11-10 11:06:06 +0300

    blogDetails component

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
-
+const BlogDetail = ({ blog }) => {
+    return (
+      <div>
+        <h2>{blog.title}</h2>
+        <p>{blog.content}</p>
+      </div>
+    );
+  }
 
 
 export default BlogDetails;
