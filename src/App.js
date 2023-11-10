dd0ce672b829c7779dfff38aaefc56126d32d1c9
alexander <alexandernyaga1999@gmail.com>
2023-11-10 11:01:23 +0300

    add components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import BlogList from './Bloglist';
+import BlogDetail from './BlogDetail';
+import AddBlog from './AddBlog';
+
+
+
+
+
+
+
+
+
+export default App;
