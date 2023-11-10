7a7f9a579f59492a7a4d804f1bf8b15f40ec5d27
alexander <alexandernyaga1999@gmail.com>
2023-11-10 11:08:39 +0300

    add bloglist function component

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
+const BlogList = ({ blogs, onDelete }) => {
+    return (
+      <div>
+        <h2>Blog List</h2>
+        <ul>
+          {blogs.map(blog => (
+            <li key={blog.id}>
+              <Link to={`/blog/${blog.id}`}>{blog.title}</Link>
+              <button onClick={() => onDelete(blog.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+          }
 
 
 
