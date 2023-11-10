33360b5ab155f6b4bcad1f461f5ef9f1a48dee1d
alexander <alexandernyaga1999@gmail.com>
2023-11-10 11:02:38 +0300

    Add app component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,23 @@ import BlogList from './Bloglist';
 import BlogDetail from './BlogDetail';
 import AddBlog from './AddBlog';
 
-
+  function App() {
+    const [blogs, setBlogs] = useState([]);
+  
+    useEffect(() => {
+      fetch('http://localhost:3001/blogs')
+        .then(response => response.json())
+        .then(data => setBlogs(data));
+    }, []);
+  
+    const handleDelete = (id) => {
+      fetch(`http://localhost:3001/blogs/${id}`, {
+        method: 'DELETE',
+      }).then(() => {
+        setBlogs(blogs.filter(blog => blog.id !== id));
+      });
+    };
+}
 
 
 
