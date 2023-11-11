44745458619f8e68ad26cf0062c27f442bbc2f70
alexander <alexandernyaga1999@gmail.com>
2023-11-11 11:53:40 +0300

    handleSearch

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,23 @@
 import React, { useState } from 'react';
 
+const Search = ({ onSearch }) => {
+  const [searchQuery, setSearchQuery] = useState('');
 
+  const handleSearch = () => {
+    onSearch(searchQuery);
+  };
+
+  return (
+    <div className="search-container">
+      <input
+        type="text"
+        placeholder="Search for a blog"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
 
 export default Search;
